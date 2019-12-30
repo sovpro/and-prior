271b82d81b0c7121da6d9d4cbd4e8d83fb383cc4
sovpro <its.so@outlook.com>
2019-12-30 12:56:06 -0500

    Update test with output

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var andPrior = require ('./../')
 var assert = require ('assert')
+var error_count = 0
 
 testBasics ()
 testPromise ()
@@ -13,25 +14,42 @@ function promiseSum (a, b) {
   return Promise.resolve (sum (a, b))
 }
 
+function assertWithInfo (value, message) {
+  process.stdout.write (message)
+  try {
+    assert (value, message)
+    console.log (' ... OK')
+  }
+  catch (error) {
+    console.log (' ... FAIL')
+    console.error (error)
+    error_count += 1
+  }
+}
+
+process.once ('exit', function (code) {
+  process.exit (Math.min (1, error_count))
+})
+
 function testBasics () {
   var sumAP = andPrior (sum)
   var value_ap = sumAP (1, 1)
 
-  assert (
+  assertWithInfo (
       value_ap.sameAsPrior () === false
-    , 'sameAsPrior() should return false for the first computed value'
+    ,'sameAsPrior() should return false for the first computed value'
   )
 
   value_ap = sumAP (1.1, 1.1)
 
-  assert (
+  assertWithInfo (
       value_ap.sameAsPrior () === false
     , 'sameAsPrior() should return false when the prior computed value is different'
   )
 
   value_ap = sumAP (1.1, 1.1)
 
-  assert (
+  assertWithInfo (
       value_ap.sameAsPrior () === true
     , 'sameAsPrior() should return true when the prior computed value is the same'
   )
@@ -41,21 +59,21 @@ function testPromise () {
   var promiseSumAP = andPrior (promiseSum)
 
   promiseSumAP (1, 1).then (function (value_ap) {
-    assert (
+    assertWithInfo (
         value_ap.sameAsPrior () === false
       , 'sameAsPrior() should return false for the first fulfilled promise value'
     )
   })
 
   promiseSumAP (1, 1).then (function (value_ap) {
-    assert (
+    assertWithInfo (
         value_ap.sameAsPrior () === true
       , 'sameAsPrior() should return true for a fulfilled promise value that that is the same as the prior promise fulfillment'
     )
   })
 
   promiseSumAP (1, 2).then (function (value_ap) {
-    assert (
+    assertWithInfo (
         value_ap.sameAsPrior () === false
       , 'sameAsPrior() should return false for a fulfilled promise value that is different from the prior promise fulfillment'
     )
@@ -77,14 +95,14 @@ function testCustomization () {
   var value_ap = sumAPCustom (1, 1)
   value_ap = sumAPCustom (1.1, 1.1)
 
-  assert (
+  assertWithInfo (
       value_ap.sameAsPrior () === true
     , 'sameAsPrior() should be able to be customized'
   )
 
   delete customConfig.sameAsPrior
 
-  assert (
+  assertWithInfo (
       value_ap.sameAsPrior () === false
     , 'sameAsPrior() should be able to be uncustomized'
   )
